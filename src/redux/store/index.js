0f41b51c1407c4da4f88f8991771198d58bb4c6f
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2021-12-25 15:30:38 +0700

    add persist reducer

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,16 +1,28 @@
 import { applyMiddleware, createStore } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+
 import rootReducer from "./reducers";
 
-export default function configureStore(preloadedState) {
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["checkout"],
+};
+
+export default function configureStore() {
   const middlewares = [thunkMiddleware];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
   const enhancers = [middlewareEnhancer];
   const composedEnhancers = composeWithDevTools(...enhancers);
 
-  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-  return store;
+  const store = createStore(persistedReducer, composedEnhancers);
+  const persistor = persistStore(store);
+
+  return { store, persistor };
 }
